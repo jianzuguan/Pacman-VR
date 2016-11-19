2fad4ab764b7f48497679c7ccd9db0f2ddc34abf
Kang Cheng <me@chengkang.pw>
2016-11-19 12:06:23 +0000

    new

---
+++ Assets/Scripts/MoveRight.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 move = new Vector3(10f, 0f, 0f);
+		gameObject.GetComponent<Rigidbody>().AddForce (move);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
